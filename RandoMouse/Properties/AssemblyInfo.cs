11acbf4a7996bb32cf174968cee98a23fba90fd3
Christian Ramelow <info@codekandis.net>
2022-07-10 18:30:39 +0200

    Initial commit.

---
+++ RandoMouse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "CodeKandis.RandoMouse" )]
+[assembly: AssemblyDescription( "`CodeKandis.RandoMouse` is an application to move a mouse randomly on the screen." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Christian Ramelow <info@codekandis.net>" )]
+[assembly: AssemblyProduct( "CodeKandis.RandoMouse" )]
+[assembly: AssemblyCopyright( "Copyright © codekandis 2022-07" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "99be3bae-25f8-44cb-b955-e86bccda1660" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "0.0.0.0" )]
+[assembly: AssemblyFileVersion( "0.0.0.0" )]
