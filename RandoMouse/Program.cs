56b7d1cad36ab9dc653512bfe04e97d4d1b46ac2
Christian Ramelow <info@codekandis.net>
2022-07-10 18:30:39 +0200

    Initial commit.

---
+++ RandoMouse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CodeKandis.RandoMouse
+{
+	/// <summary>
+	/// Represents the bootstrap class of the application.
+	/// </summary>
+	static class Program
+	{
+		/// <summary>
+		/// Represents the bootstrap method of the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault( false );
+			Application.Run();
+		}
+	}
+}
